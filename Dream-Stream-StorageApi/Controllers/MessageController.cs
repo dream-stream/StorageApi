a18920707241a927e6ac6d8ba421fe859616b780
Nicklas <nnicklas92@gmail.com>
2019-12-05 19:16:00 +0100

    Error handling

--- Dream-Stream-StorageApi/Controllers/MessageController.cs
+++ Dream-Stream-StorageApi/Controllers/MessageController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using MessagePack;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Prometheus;
 
 namespace Dream_Stream_StorageApi.Controllers
 {
@@ -14,10 +15,18 @@ namespace Dream_Stream_StorageApi.Controllers
     public class MessageController : ControllerBase
     {
         private readonly ILogger<MessageController> _logger;
-        //private const string BasePath = "/Users/Nicklas Nielsen/Desktop/data";
         private const string BasePath = "/mnt/data";
         private static readonly SemaphoreSlim MessageLock = new SemaphoreSlim(1, 1);
         private static readonly SemaphoreSlim OffsetLock = new SemaphoreSlim(1, 1);
+        private static readonly Counter MessagesWrittenSizeInBytes = Metrics.CreateCounter("messages_written_size_in_bytes", "", new CounterConfiguration
+        {
+            LabelNames = new[] { "TopicPartition" }
+        });
+
+        private static readonly Counter MessagesReadSizeInBytes = Metrics.CreateCounter("messages_read_size_in_bytes", "", new CounterConfiguration
+        {
+            LabelNames = new[] { "TopicPartition" }
+        });
 
         public MessageController(ILogger<MessageController> logger)
         {
@@ -31,18 +40,19 @@ namespace Dream_Stream_StorageApi.Controllers
             var streamKey = $"{consumerGroup}/{topic}/{partition}";
             var stream = FileStreamHandler.GetFileStream(streamKey, filePath);
 
-            if (!stream.CanRead || !stream.CanSeek) throw new Exception("AArgghh");
-
-            await StoreOffset(consumerGroup, topic, partition, offset);
+            if (!stream.CanRead || !stream.CanSeek) throw new Exception("AArgghh Stream");
+            if (!await StoreOffset(consumerGroup, topic, partition, offset)) throw new Exception("AArgghh Offset");
+            
             var buffer = new byte[amount];
             stream.Seek(offset, SeekOrigin.Begin);
             await stream.ReadAsync(buffer);
 
+            MessagesReadSizeInBytes.WithLabels($"{topic}/{partition}").Inc(amount);
             await Response.Body.WriteAsync(buffer);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Store([FromQuery]string topic, int partition, int length)
+        public async Task<IActionResult> Store([FromQuery]string topic, int partition, int length, int recursiveCount = 0)
         {
             var filePath = $"{BasePath}/{topic}/{partition}.txt";
             var streamKey = $"{topic}/{partition}";
@@ -59,13 +69,25 @@ namespace Dream_Stream_StorageApi.Controllers
             if (buffer[^1] != 67) return StatusCode(500);
 
             await MessageLock.WaitAsync();
-            stream.Seek(0, SeekOrigin.End);
-            var offset = stream.Position;
+            var offset = -1L;
+            try
+            {
+                stream.Seek(0, SeekOrigin.End);
+                offset = stream.Position;
 
-            await stream.WriteAsync(lengthInBytes);
-            await stream.WriteAsync(buffer.ToArray());
-            MessageLock.Release();
+                await stream.WriteAsync(lengthInBytes);
+                await stream.WriteAsync(buffer.ToArray());
+                MessageLock.Release();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                MessageLock.Release();
+                if (recursiveCount > 2) return StatusCode(500);
+                await Store(topic, partition, length, ++recursiveCount);
+            }
 
+            MessagesWrittenSizeInBytes.WithLabels($"{topic}/{partition}").Inc(length);
             return Ok(offset);
         }
 
@@ -85,18 +107,31 @@ namespace Dream_Stream_StorageApi.Controllers
             return Ok(LZ4MessagePackSerializer.Deserialize<long>(buffer));
         }
 
-        private async Task StoreOffset(string consumerGroup, string topic, int partition, long offset)
+        private async Task<bool> StoreOffset(string consumerGroup, string topic, int partition, long offset, int recursiveCount = 0)
         {
             var filePath = $"{BasePath}/offsets/{topic}/{partition}.txt";
             var streamKey = $"offset/{consumerGroup}/{topic}/{partition}";
             var stream = FileStreamHandler.GetFileStream(streamKey, filePath);
 
-            if (!stream.CanWrite || !stream.CanRead) return;
+            if (!stream.CanWrite || !stream.CanRead) return false;
 
             await OffsetLock.WaitAsync();
-            stream.Seek(0, SeekOrigin.Begin);
-            await stream.WriteAsync(LZ4MessagePackSerializer.Serialize(offset));
-            OffsetLock.Release();
+            try
+            {
+                stream.Seek(0, SeekOrigin.Begin);
+                await stream.WriteAsync(LZ4MessagePackSerializer.Serialize(offset));
+                OffsetLock.Release();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                OffsetLock.Release();
+                
+                if (recursiveCount > 2) return false;
+                
+                return await StoreOffset(consumerGroup, topic, partition, offset, ++recursiveCount);
+            }
         }
     }
 }

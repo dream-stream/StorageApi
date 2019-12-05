8932d750e5b7fe6331f0e23bd45dfbdc3b4c2c26
Nicklas <nnicklas92@gmail.com>
2019-12-05 20:36:31 +0100

    Prometheus

--- Dream-Stream-StorageApi/Startup.cs
+++ Dream-Stream-StorageApi/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Prometheus;
 
 namespace Dream_Stream_StorageApi
 {
@@ -33,6 +34,7 @@ namespace Dream_Stream_StorageApi
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseMetricServer();
             //app.UseSwagger();
 
             //app.UseSwaggerUI(c =>

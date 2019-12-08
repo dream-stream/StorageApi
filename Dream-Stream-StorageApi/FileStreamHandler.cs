2e3c5b7ec87bab0d28c8923a02f522b6ae2c998c
Nicklas <nnicklas92@gmail.com>
2019-12-08 15:20:38 +0100

    Removal of file handling

--- Dream-Stream-StorageApi/FileStreamHandler.cs
+++ Dream-Stream-StorageApi/FileStreamHandler.cs
@@ -7,24 +7,25 @@ namespace Dream_Stream_StorageApi
 {
     public static class FileStreamHandler
     {
-        private static readonly ConcurrentDictionary<string, (Timer timer, FileStream fileStream)> FileStreams = new ConcurrentDictionary<string, (Timer timer, FileStream fileStream)>();
+        //private static readonly ConcurrentDictionary<string, (Timer timer, FileStream fileStream)> FileStreams = new ConcurrentDictionary<string, (Timer timer, FileStream fileStream)>();
+        private static readonly ConcurrentDictionary<string, FileStream> FileStreams = new ConcurrentDictionary<string, FileStream>();
         private static readonly SemaphoreSlim Lock = new SemaphoreSlim(1, 1);
 
         public static FileStream GetFileStream(string key, string filePath)
         {
             if(!File.Exists(filePath)) CreateFile(filePath);
 
-            var (timer, fileStream) = FileStreams.GetOrAdd(key,
-                (new Timer(x =>
-                    {
-                        if (!FileStreams.TryRemove(key, out var tuple)) return;
-                        tuple.fileStream.Close();
-                        tuple.fileStream.Dispose();
-                        tuple.timer.Dispose();
-                    }, null, 10000, 1000),
-                    new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite)));
+            var fileStream = FileStreams.GetOrAdd(key,
+                //(new Timer(x =>
+                //    {
+                //        if (!FileStreams.TryRemove(key, out var tuple)) return;
+                //        tuple.fileStream.Close();
+                //        tuple.fileStream.Dispose();
+                //        tuple.timer.Dispose();
+                //    }, null, 10000, 1000),
+                    new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite));
             
-            timer.Change(10000, 1000);
+            //timer.Change(10000, 1000);
 
             return fileStream;
         }
